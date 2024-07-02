0dda4282b057c7994031a0829e76247e88b90b72
Jakub Maciocha <ciamcio@dellg15jm.home>
2024-07-02 09:27:59 +0200

    Basic implemenatation of menu

---
+++ app/src/main/java/menu/Menu.java
@@ -0,0 +1,89 @@
+package menu;
+
+import java.io.*;
+
+import com.google.j2objc.annotations.Weak;
+
+public final class Menu {
+    private static final String MENU_CONTECST = "------ Your trainign main menu ------\n1. Load training\n2. Start training\n3. Exit application\n";
+    private static final int MENU_SIZE = 3, BREAK_HADNLING_INPUT = -1 ;
+    private static Menu instance;
+    private static final InputStream inputStream = System.in;
+
+    private Menu() {
+    }
+
+    public static Menu getInstance() {
+        if (instance == null) {
+            instance = new Menu();
+        }
+        return instance;
+    }
+
+    public void menuRunner() {
+        int input = -1; 
+        boolean shouldStop = false;
+        
+        do {
+            System.out.println(MENU_CONTECST);
+            input = handleInput(new InputStreamReader(inputStream)); 
+            shouldStop = performeMenuAction(input);
+
+        } while(shouldStop);
+    }
+
+    /**
+     * Method handles the value recived from the input stream. The value from the stream will be retuned in case it is correct. The second requirment is to provide the correct value in no more than 3 attmepts, after that handling of the function will stop.
+     * @param stream input stream which is the source of an input
+     * @return  in case of correct input the value of input is returned, if 3 attempts of reading from stream will be unsucesful hadnling of the input is terminated by returning -1
+     */
+    private int handleInput(InputStreamReader stream) {
+        boolean isNotValid = false;
+        int input = -1, attempts = 3; 
+        do {
+            input = getInput(stream); 
+            isNotValid = !validateInput(input);
+        } while(isNotValid && attempts-- > 0 );
+        if (isNotValid)
+            return BREAK_HADNLING_INPUT;
+        return input;
+    }
+
+    /**
+     * Method gets integer value from provided input stream which is input
+     * @param stream input stream which is the source of input 
+     * @return returns the value from the stream as an Integer. In case the value cannot be converted to Integer 0 is returned.
+     */
+    private int getInput(InputStreamReader stream) {
+        BufferedReader reader = new BufferedReader(stream);
+        System.out.println("> ");
+        try {
+            return Integer.parseInt(reader.readLine());
+        } catch(Exception e) {
+            return 0;
+        }
+    }
+
+    /**
+     * Method validates input provided as an argument if it matches the range of Menu options
+     * @param input input provided from the specific input stream 
+     * @return ture if input matches the range of Menu, false in other case
+     */
+    private boolean validateInput(int input) {
+        if (input > 0 && input <= MENU_SIZE) {
+            return true;
+        }  
+        return false;
+    }
+
+    private boolean performeMenuAction(int input) {
+        switch(input) {
+            case 1 -> { return false; }
+            case 2 -> { return false; } 
+            case 3 -> { return true; }
+        }
+        return false;
+    }
+
+    
+}
---
+++ app/src/test/java/menu/MenuTest.java
@@ -0,0 +1,129 @@
+package menu;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import java.lang.reflect.Method;
+import java.io.*;
+import java.util.*;
+
+class MenuTest {
+    private static Menu menu = Menu.getInstance();
+    private static final Method[] privMethods = Menu.class.getDeclaredMethods();
+
+    @Test 
+    void singletonMenuTest() {
+        assertNotNull(menu);
+        Menu testMenu = Menu.getInstance(); 
+        assertEquals(menu, testMenu);
+    }
+
+    @Test
+    void handleInputTest() {
+        String userInput = "1\n";
+        int resultInput = -1;
+        Method handleInput = findPrivateMethod("handleInput");
+        if (handleInput == null) 
+           fail("Method not found"); 
+        handleInput.setAccessible(true);
+        try {
+            resultInput = (Integer) handleInput.invoke(menu, new InputStreamReader(new ByteArrayInputStream(userInput.getBytes())));
+        } catch(Exception e) {
+            fail("Method shouldn't throw an exception");
+        }
+        assertEquals(1, resultInput);
+        userInput = "word\n";
+        try {
+            resultInput = (Integer) handleInput.invoke(menu, new InputStreamReader(new ByteArrayInputStream(userInput.getBytes())));
+        } catch(Exception e) {
+            fail("Method shouldn't throw an exception");
+        }
+        assertEquals(-1, resultInput);
+        handleInput.setAccessible(false);
+        handleInput = null;
+    }
+
+    @Test
+    void getInputTest() {
+        String userInput = "1\n";
+        int resultInput = -1;
+        Method getInput = findPrivateMethod("getInput");
+        if (getInput == null)
+            fail("Method not found");
+        getInput.setAccessible(true);
+        try {
+            resultInput = (Integer) getInput.invoke(menu, new InputStreamReader(new ByteArrayInputStream(userInput.getBytes())));
+        } catch(Exception e) {
+            fail("Method shouldn't throw an excpetion");
+        }
+        assertEquals(1, resultInput);
+        userInput = "word\n";
+        try {
+            resultInput = (Integer) getInput.invoke(menu, new InputStreamReader(new ByteArrayInputStream(userInput.getBytes())));
+        } catch(Exception e) {
+            fail("Method shouldn't throw an exception");
+        }
+        assertEquals(0, resultInput);
+        getInput.setAccessible(false);
+        getInput = null;
+    }
+
+    @Test
+    void validateInputTest() {
+        int validInput = 1, negativeInput = -1, zeroInput  = 0, oversizeInput = Integer.MAX_VALUE;
+        List<Boolean> validationResults = new ArrayList<>(); 
+        Method validateInput = findPrivateMethod("validateInput");
+        if (validateInput == null)
+            fail("Method not found");
+        validateInput.setAccessible(true);
+        try {
+            validationResults.add((Boolean) validateInput.invoke(menu, validInput));
+            validationResults.add((Boolean) validateInput.invoke(menu, negativeInput));
+            validationResults.add((Boolean) validateInput.invoke(menu, zeroInput));
+            validationResults.add((Boolean) validateInput.invoke(menu, oversizeInput));
+        } catch(Exception e) {
+            fail("Method shouldn't throw an expception");      
+        }
+
+        assertEquals(true, validationResults.get(0));
+        assertEquals(false, validationResults.get(1));
+        assertEquals(false, validationResults.get(2));
+        assertEquals(false, validationResults.get(3));
+        validateInput.setAccessible(false);
+        validateInput = null;
+    }
+
+    @Test
+    void performeMenuActionTest() {
+        boolean result = false;
+        int defaultInput = 1, closingActionInput = 3;
+        Method performeMenuAction = findPrivateMethod("performeMenuAction");
+        if (performeMenuAction == null)
+            fail("Method not found");
+        performeMenuAction.setAccessible(true);
+        try {
+            result = (Boolean) performeMenuAction.invoke(menu, defaultInput);
+        } catch(Exception e) {
+            fail("Method shouldn't throw an exception");
+        }
+        assertEquals(false, result);
+        try {
+            result = (Boolean) performeMenuAction.invoke(menu, closingActionInput);
+        } catch(Exception e) {
+            fail("Method shouldn't throw an excpetion");
+        }
+        assertEquals(true, result);
+        performeMenuAction.setAccessible(false);
+        performeMenuAction = null;
+    }
+
+
+    Method findPrivateMethod(String methodName) {
+        for (Method method : privMethods) {
+            if (method.getName().equals(methodName))
+                return method;
+        }
+        return null;
+    }
+
+}
