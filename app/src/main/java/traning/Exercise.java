adb738358f9d4a5f7e33442309e844d4008476a6
Jakub Maciocha <ciamcio@dellg15jm.home>
2024-07-14 13:18:03 +0200

    Exercises class documentation update

--- app/src/main/java/traning/Exercise.java
+++ app/src/main/java/traning/Exercise.java
@@ -11,15 +11,32 @@ class Exercise {
     private long executionTime;
     private String description;
 
+    /**
+     * Constructor creates the exercise object based on the string object passed as an argument which describes object  
+     * @param exercisesDescription line describing the object variables 
+     */
     protected Exercise(String exercisesDescription) {
         List<String> exerciseData = new ArrayList<String>(Arrays.asList(exercisesDescription.split(",")));
         if (exerciseData.size() != EXERCISE_INFORMATION_IN_LINE) 
-            fillExercisesInformationSizeDifference(exerciseData);
+            lineExerciseInformationSizeDifference(exerciseData);
         setName(exerciseData.get(0));
         setExecutionTime(Long.parseLong(exerciseData.get(1)));
         setDescription(exerciseData.get(2));
     }
 
+    /**
+     * Function validates number of information contained in line, in case of missing infomration list is filled with empty records.   
+     * @param exerciseData list containing the single line representing the one exercise 
+     * @return integer representing the amount of missing data in the line representing exercise
+     */
+    private int lineExerciseInformationSizeDifference(List<String> exerciseData) {
+        int sizeDifference = EXERCISE_INFORMATION_IN_LINE - exerciseData.size();  
+        for (int i = 0; i < sizeDifference; i++)
+            exerciseData.add("");
+        return sizeDifference;
+    }
+
+// GETTERS
     protected String getName() {
         return name;
     }
@@ -30,12 +47,22 @@ class Exercise {
         return description;
     }
 
+// SETTERS
+    /**
+     * Name variable exercise setter for exerciese object. If the vlaue passed as an argument is not empty the exercise's name is set.
+     * @param name exercise name provided by the user 
+     */
     protected void setName(String name) {
         if (!name.equals(""))
             this.name = name;
         else
             this.name = DEFAULT_NAME;
     }
+
+    /**
+     * Execution time variable setter for exerciese object. If the argument is within the range of zero to Integer max then the execution time is set.  
+     * @param executionTime execution time provided by the user 
+     */
     protected void setExecutionTime(long executionTime) {
         if (executionTime < Integer.MAX_VALUE && executionTime > 0)
             this.executionTime = executionTime;
@@ -48,12 +75,5 @@ class Exercise {
         else
             this.description = DEFAULT_DESCRIPTION;
         
-    }
-
-    private int fillExercisesInformationSizeDifference(List<String> exerciseData) {
-        int sizeDifference = EXERCISE_INFORMATION_IN_LINE - exerciseData.size();  
-        for (int i = 0; i < sizeDifference; i++)
-            exerciseData.add("");
-        return sizeDifference;
     }
 }
