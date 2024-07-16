9d4e53dd36fff199c27ef77f68005c21546ceb40
Jakub Maciocha <ciamcio@dellg15jm.home>
2024-07-16 10:22:42 +0200

    Documnatation update TraningManagment interface

--- app/src/main/java/traning/TraningManagment.java
+++ app/src/main/java/traning/TraningManagment.java
@@ -1,6 +1,12 @@
 package traning;
 
 public interface TraningManagment {
+    /**
+     * Method responisble for loading traning units from the source provided as a path. 
+     * @param pathToTraningFile string object representing the path to the source of traning data
+     * @return method returns true if the process of loading traning was successfull, in any other case method returns false 
+     */
     public boolean loadTraning(String pathToTraningFile);
+
     public void procesTraining();  
 }
