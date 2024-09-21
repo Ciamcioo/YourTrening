b6aa3f2d5fdacd5faaad590e508ad3beb6b76d83
Jakub Maciocha <ciamcio@dellg15jm.home>
2024-09-21 18:19:20 +0200

    New gui branch

--- app/src/main/java/yourtraining/App.java
+++ app/src/main/java/yourtraining/App.java
@@ -8,6 +8,7 @@ public class App {
     public App() {
         menu = Menu.getInstance();
     }
+
     public static void main(String[] args) {
         App app = new App();
         app.menu.menuRunner();
